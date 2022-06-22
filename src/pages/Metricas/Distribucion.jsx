62de5b9dc52e1708637d051872f9937a55d8e3e1
Gustavo Caradonna <gustavo@datawebhosting.com.ar>
2022-06-22 15:52:09 -0300

    distribucion ok, falta cambiar endpoint y ajustar

--- src/pages/Metricas/Distribucion.jsx
+++ src/pages/Metricas/Distribucion.jsx
@@ -9,8 +9,43 @@ import {
   Inject,
 } from "@syncfusion/ej2-react-charts";
 import { pieChartData } from "../../data/dummy";
+import { useEffect, useState } from "react";
 
 const Distribucion = () => {
+  // const [hoteles, setHoteles] = useState([]);
+  const [hotel, setHotel] = useState([]);
+  const [residencial, setResidencial] = useState([]);
+  const [hosteria, setHosteria] = useState([]);
+  const [apartHotel, setApartHotel] = useState([]);
+
+  useEffect(() => {
+    // const endPoint = "http://localhost:3009/api/fedata/hoteles";
+    obtenerDatos();
+  }, []);
+
+  const obtenerDatos = async () => {
+    const data = await fetch("http://localhost:3009/api/fedata/hoteles");
+    const data2 = await data.json();
+
+    let mostrar = data2[0];
+    console.log(mostrar);
+
+    // setHoteles(mostrar);
+    console.log(mostrar[0].Categoria.value);
+    setHotel(mostrar[0].Categoria.value);
+    setResidencial(10);
+    setHosteria(20);
+    setApartHotel(30);
+  };
+
+  const pieChartData2 = [
+    // { x: "Hotel", y: hotel, text: "18%" },
+    { x: "Hotel", y: hotel, text: hotel + "%" },
+    { x: "Residencial", y: residencial, text: residencial + "%" },
+    { x: "Hostería", y: hosteria, text: hosteria + "%" },
+    { x: "Apart Hotel", y: apartHotel, text: apartHotel + "%" },
+  ];
+
   return (
     <div className="md:m-10 m-4 md:mt-40 mt-20">
       <div className="w-full">
@@ -29,7 +64,7 @@ const Distribucion = () => {
           <AccumulationSeriesCollectionDirective>
             <AccumulationSeriesDirective
               name="Project"
-              dataSource={pieChartData}
+              dataSource={pieChartData2}
               xName="x"
               yName="y"
               innerRadius="40%"
