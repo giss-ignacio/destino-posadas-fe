18c1d1863d28b944059757b8a659dfbdeaa36b97
Gustavo Caradonna <gustavo@datawebhosting.com.ar>
2022-06-24 01:01:39 -0300

    fix

--- src/pages/Metricas/Total.jsx
+++ src/pages/Metricas/Total.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   ChartComponent,
   SeriesCollectionDirective,
@@ -8,31 +8,26 @@ import {
   DateTime,
   Legend,
   Tooltip,
-} from '@syncfusion/ej2-react-charts';
-import { lineChartData } from '../../data/dummy';
-
+} from "@syncfusion/ej2-react-charts";
+import { lineChartData } from "../../data/dummy";
 
 const Viajeros = () => {
-  state = {
-    persons: []
-  }
-
   return (
-    <div className='md:m-10 m-4 md:mt-40 mt-20 '>
-      <div className='w-full'>
+    <div className="md:m-10 m-4 md:mt-40 mt-20 ">
+      <div className="w-full">
         <ChartComponent
-          id='charts'
-          style={{ textAlign: 'center' }}
+          id="charts"
+          style={{ textAlign: "center" }}
           primaryXAxis={{
-            valueType: 'DateTime',
-            labelFormat: 'y',
-            intervalType: 'Years',
-            edgeLabelPlacement: 'Shift',
+            valueType: "DateTime",
+            labelFormat: "y",
+            intervalType: "Years",
+            edgeLabelPlacement: "Shift",
             majorGridLines: { width: 0 },
           }}
           primaryYAxis={{
-            labelFormat: '{value}%',
-            rangePadding: 'None',
+            labelFormat: "{value}%",
+            rangePadding: "None",
             minimum: 0,
             maximum: 100,
             interval: 20,
@@ -42,27 +37,27 @@ const Viajeros = () => {
           }}
           chartArea={{ border: { width: 0 } }}
           tooltip={{ enable: true }}
-          title='Inflation - Consumer Price'
+          title="Inflation - Consumer Price"
         >
           <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
           <SeriesCollectionDirective>
             <SeriesDirective
               dataSource={lineChartData[0]}
-              xName='x'
-              yName='y'
-              name='Germany'
+              xName="x"
+              yName="y"
+              name="Germany"
               width={2}
               marker={{ visible: true, width: 10, height: 10 }}
-              type='Line'
+              type="Line"
             ></SeriesDirective>
             <SeriesDirective
               dataSource={lineChartData[1]}
-              xName='x'
-              yName='y'
-              name='England'
+              xName="x"
+              yName="y"
+              name="England"
               width={2}
               marker={{ visible: true, width: 10, height: 10 }}
-              type='Line'
+              type="Line"
             ></SeriesDirective>
           </SeriesCollectionDirective>
         </ChartComponent>
