ec6bc2dffef7ecdb163e1782d468d7fcef8af271
dario <dpiazza@gmail.com>
2022-06-23 23:02:25 -0300

    Metricas Wifi anual por tipo

--- src/pages/Metricas/RRSS.jsx
+++ src/pages/Metricas/RRSS.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+//import React from 'react';
 import {
   ChartComponent,
   SeriesCollectionDirective,
@@ -8,9 +8,97 @@ import {
   SplineAreaSeries,
   Legend,
 } from '@syncfusion/ej2-react-charts';
-import { areaChartData } from '../../data/dummy';
+
+import {React, useEffect, useState } from "react";
 
 const RRSS = () => {
+  const [valor, setValor] = useState([]);
+
+var x
+useEffect(() => {
+  obtenerDatos();
+}, []);
+let m = "Enero";
+
+const obtenerDatos = async () => {
+  const data = await fetch("http://localhost:3009/api/fedata/getServicioPorMes?a=2022&m=Enero&c=WiFi&t=Hotel");
+  //const data = await fetch("http://localhost:3009/api/fedata/getServicioPorMes?a=2022&m="+m+"&c=WiFi&t=Hotel");
+  const data2 = await data.json();
+  setValor(data2[0])
+  x = data2
+  console.log("Valor de data2 :"+data2[0])
+};
+console.log("Valor x :" + x)
+var miArray = []
+miArray.push(m)
+console.log(miArray)
+console.log("Valor :"+valor)
+
+var respuesta =
+[
+    [
+    //Verde  Hoteles
+    { x: new Date(2022, 1, 1), y: 9 },
+    { x: new Date(2022, 2, 1), y: 3.4 },
+    { x: new Date(2022, 3, 1), y: 3.4 },
+    { x: new Date(2022, 4, 1), y: 1.6 },
+    { x: new Date(2022, 5, 1), y: 2.3 },
+    { x: new Date(2022, 6, 1), y: 2.5 },
+    { x: new Date(2022, 7, 1), y: 2.9 },
+    { x: new Date(2022, 8, 1), y: 3.8 },
+    { x: new Date(2022, 9, 1), y: 1.4 },
+    { x: new Date(2022, 10, 1), y: 3.1 },
+    { x: new Date(2022, 11, 1), y: 9 },
+    { x: new Date(2022, 12, 1), y: 9 },
+  ],
+  [
+    //Verde  Recidencial
+    { x: new Date(2022, 1, 1), y: 5 },
+    { x: new Date(2022, 2, 1), y: 3.4 },
+    { x: new Date(2022, 3, 1), y: 5.4 },
+    { x: new Date(2022, 4, 1), y: 1.6 },
+    { x: new Date(2022, 5, 1), y: 5.3 },
+    { x: new Date(2022, 6, 1), y: 2.5 },
+    { x: new Date(2022, 7, 1), y: 5.9 },
+    { x: new Date(2022, 8, 1), y: 3.8 },
+    { x: new Date(2022, 9, 1), y: 1.4 },
+    { x: new Date(2022, 10, 1), y: 3.1 },
+    { x: new Date(2022, 11, 1), y: 9 },
+    { x: new Date(2022, 12, 1), y: 9 },
+  ],
+  [
+    //Gris APART
+    { x: new Date(2022, 1, 1), y: 9 },
+    { x: new Date(2022, 2, 1), y: 9 },
+    { x: new Date(2022, 3, 1), y: 9 },
+    { x: new Date(2022, 4, 1), y: 9 },
+    { x: new Date(2022, 5, 1), y: 5 },
+    { x: new Date(2022, 6, 1), y: 5 },
+    { x: new Date(2022, 7, 1), y: 5 },
+    { x: new Date(2022, 8, 1), y: 8 },
+    { x: new Date(2022, 9, 1), y: 8 },
+    { x: new Date(2022, 10, 1), y: 8 },
+    { x: new Date(2022, 11, 1), y: 9 },
+    { x: new Date(2022, 12, 1), y: 9 },
+  ],
+  [
+    //azul
+    { x: new Date(2022, 1, 1), y: 0.8 },
+    { x: new Date(2022, 2, 1), y: 1.3 },
+    { x: new Date(2022, 3, 1), y: 1.1 },
+    { x: new Date(2022, 4, 1), y: 1.6 },
+    { x: new Date(2022, 5, 1), y: 2 },
+    { x: new Date(2022, 6, 1), y: 1.7 },
+    { x: new Date(2022, 7, 1), y: 2.3 },
+    { x: new Date(2022, 8, 1), y: 2.7 },
+    { x: new Date(2022, 9, 1), y: 1.1 },
+    { x: new Date(2022, 10, 1), y: 2.3 },
+    { x: new Date(2022, 11, 1), y: 9 },
+    { x: new Date(2022, 12, 1), y: 9 },
+  ],
+];
+
+//const RRSS = () => {
   return (
     <div className='md:m-10 m-4 md:mt-40 mt-20'>
       <div className='w-full'>
@@ -19,47 +107,57 @@ const RRSS = () => {
           style={{ textAlign: 'center' }}
           primaryXAxis={{
             valueType: 'DateTime',
-            labelFormat: 'y',
+            labelFormat: 'y/MM',
             majorGridLines: { width: 0 },
-            intervalType: 'Years',
+            intervalType: 'Months',
+            //intervalType: 'Years',
             edgeLabelPlacement: 'Shift',
           }}
           primaryYAxis={{
-            labelFormat: '{value}%',
+            labelFormat: '{value}',
             lineStyle: { width: 0 },
-            maximum: 4,
+            maximum: 10,
             interval: 1,
             majorTickLines: { width: 0 },
             minorTickLines: { width: 0 },
           }}
           chartArea={{ border: { width: 0 } }}
-          title='Inflation Rate in Percentage'
+          title='AV Valoración WiFi por tipo de Alojamiento'
         >
           <Inject services={[SplineAreaSeries, DateTime, Legend]} />
           <SeriesCollectionDirective>
             <SeriesDirective
-              dataSource={areaChartData[0]}
+              dataSource={respuesta[0]}
               xName='x'
               yName='y'
-              name='US'
+              name='Hotel'
               opacity={0.5}
               type='SplineArea'
               width={2}
             ></SeriesDirective>
             <SeriesDirective
-              dataSource={areaChartData[1]}
+              dataSource={respuesta[1]}
               xName='x'
               yName='y'
-              name='France'
+              name='Residencial'
               opacity={0.5}
               type='SplineArea'
               width={2}
             ></SeriesDirective>
             <SeriesDirective
-              dataSource={areaChartData[2]}
+              dataSource={respuesta[2]}
               xName='x'
               yName='y'
-              name='Germany'
+              name='Apart Hotel'
+              opacity={0.5}
+              type='SplineArea'
+              width={2}
+            ></SeriesDirective>
+            <SeriesDirective
+              dataSource={respuesta[3]}
+              xName='x'
+              yName='y'
+              name='Hosteria'
               opacity={0.5}
               type='SplineArea'
               width={2}
